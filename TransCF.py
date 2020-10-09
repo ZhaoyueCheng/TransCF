2bdac7134e6ea3d8539ec3f38cd21cab63a8c5e0
Joey <joey@layer6.ai>
2020-10-09 14:35:07 -0400

    tuning for yelp

--- TransCF.py
+++ TransCF.py
@@ -11,6 +11,7 @@ from torch.utils.data import DataLoader
 import random
 import fastrand
 from tqdm import tqdm
+from tqdm import trange
 
 class TransCF(Recommender):
     def __init__(self, args):
@@ -51,13 +52,13 @@ class TransCF(Recommender):
         # bestHR = 0
         # bestNDCG = 0
         # early_stop_metric = []
-        for epoch in range(self.numEpoch):
+        for epoch in trange(self.numEpoch):
             totalLoss = 0
             # Reading Data
             totalData = self.getTrainInstances()
             train_by_dataloader = Dataset_TransCF(totalData)
             train_loader = DataLoader(dataset=train_by_dataloader, batch_size=self.batch_size, shuffle=True)
-            for batch_idx, batch in enumerate((train_loader)):
+            for batch_idx, batch in enumerate(tqdm(train_loader)):
                 u = torch.LongTensor(batch['u'])
                 i = torch.LongTensor(batch['i'])
                 j = torch.LongTensor(batch['j'])
@@ -95,7 +96,7 @@ class TransCF(Recommender):
             model.itemEmbed.weight.data.div_(torch.max(torch.norm(model.itemEmbed.weight.data, 2, 1, True), self.clip_max).expand_as(model.itemEmbed.weight.data))
             
             # Evaluate the performance every three iterations (for running time issue)
-            if (epoch + 1) % 100 == 0 and epoch > 400:
+            if (epoch + 1) % 100 == 0 and epoch > 200:
                 model.eval()
                 results = self.evalScore(model)
                 model.train()
